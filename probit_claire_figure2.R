0f90beeb9a92444ac6622f7eed01d2bb5b117713
bbarres <benoit.barres@gmail.com>
2016-04-26 18:03:49 +0200

    new figure 2 for Claire's paper

---
+++ probit_claire_figure2.R
@@ -0,0 +1,22 @@
+###############################################################################
+###############################################################################
+#the R code for the Figure 2 of Claire's paper
+###############################################################################
+###############################################################################
+
+#set the working directory
+setwd("~/work/Rfichiers/Githuber/probit_data")
+
+
+###############################################################################
+#plotting the LC50 with imdaclopride against LC50 with thiaclopride
+###############################################################################
+
+op<-par(mar=c(5.1,5.1,2.1,2.1))
+ITdata<-read.table("imida-thia.txt",header=TRUE,sep="\t")
+plot(ITdata,log="xy",pch=19,bg="black",cex=1.5,cex.lab=1.5,las=1,
+     xlab=expression(paste("imidacloprid LC50 ",µg.liter^-1)),
+     ylab=expression(paste("thiachloprid LC50 ",µg.liter^-1)))
+rezlm<-lm(log10(ITdata$thiacloprid.LC50)~log10(ITdata$imidacloprid.LC50))
+abline(rezlm,col="grey60",lwd=2)
+par(op)
