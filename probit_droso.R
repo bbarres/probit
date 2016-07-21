bc6e56c91894fbafe440dcc25646d0036e122f0f
bbarres <benoit.barres@gmail.com>
2016-07-22 01:08:13 +0200

    new analysis of the drosophila data

---
+++ probit_droso.R
@@ -0,0 +1,38 @@
+###############################################################################
+###############################################################################
+#determining IC50, ED50, DL50 ...
+###############################################################################
+###############################################################################
+
+#loading the libraries
+library(drc)
+library(plotrix)
+
+#set the working directory
+setwd("~/work/Rfichiers/Githuber/probit_data")
+
+
+
+###############################################################################
+#example for Venturia inaequalis resistance to Difenoconazol
+###############################################################################
+
+#load the dataset
+droso<-read.table("Droso2.txt",header=T,sep="\t")
+
+#in order to obtain the same results than with priprobit, the Finney equivalent
+#method, we have to remove the constrain on lowerlimit and chose a log-normal 
+#model instead of a log-logistic model
+droso.m1<-drm(mort/total~dose,weights=total,data=droso,fct=LN.3u(),
+              type="binomial")
+plot(droso.m1)
+plot(droso.m1,type="confidence")
+
+#the ED50 obtained is identical to the one obtained with priprobit, the SD 
+#still differ a little bit
+ed50val_droso1<-ED(droso.m1,50,interval="delta",reference="control")
+
+droso.m2<-drm(mort/total~dose+age,weights=total,data=droso,fct=LN.3u(),
+              type="binomial")
+plot(droso.m2)
+plot(droso.m2,type="confidence")
